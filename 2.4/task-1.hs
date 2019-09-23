4f7f6b72647df79c5655efeddc62da7c9a6cc9f8
Nick Pavlov <npavlov@technotimes.org>
2019-09-23 12:19:59 +0300

    Add lost chapter

---
+++ 2.4/task-1.hs
@@ -0,0 +1,17 @@
+class KnownToGork a where
+    stomp :: a -> a
+    doesEnrageGork :: a -> Bool
+
+class KnownToMork a where
+    stab :: a -> a
+    doesEnrageMork :: a -> Bool
+
+class (KnownToGork a, KnownToMork a) => KnownToGorkAndMork a where
+    stompOrStab :: a -> a
+    stompOrStab a | x && y = (stomp . stab)  a
+                  | x                     = stomp a
+                  | y                     = stab a
+                  | otherwise             = a
+                     where 
+                            x = doesEnrageMork a
+                            y = doesEnrageGork a
