d7ed4c88d594b56a3431461a87148d14bc3e337b
Nick Pavlov <npavlov@technotimes.org>
2019-09-23 12:20:36 +0300

    Apply formatting

--- 2.4/task-1.hs
+++ 2.4/task-1.hs
@@ -1,17 +1,20 @@
 class KnownToGork a where
-    stomp :: a -> a
-    doesEnrageGork :: a -> Bool
+        stomp :: a -> a
+
+        doesEnrageGork :: a -> Bool
 
 class KnownToMork a where
-    stab :: a -> a
-    doesEnrageMork :: a -> Bool
+        stab :: a -> a
+
+        doesEnrageMork :: a -> Bool
 
 class (KnownToGork a, KnownToMork a) => KnownToGorkAndMork a where
-    stompOrStab :: a -> a
-    stompOrStab a | x && y = (stomp . stab)  a
-                  | x                     = stomp a
-                  | y                     = stab a
-                  | otherwise             = a
-                     where 
-                            x = doesEnrageMork a
-                            y = doesEnrageGork a
+        stompOrStab :: a -> a
+        stompOrStab a
+          | x && y = (stomp . stab) a
+          | x = stomp a
+          | y = stab a
+          | otherwise = a
+
+          where x = doesEnrageMork a
+                y = doesEnrageGork a
--- 2.4/task-3.hs
+++ 2.4/task-3.hs
@@ -1,9 +1,11 @@
 class (Eq a, Enum a, Bounded a) => SafeEnum a where
-  ssucc :: a -> a
-  ssucc x | x == maxBound = minBound
+        ssucc :: a -> a
+        ssucc x
+          | x == maxBound = minBound
           | otherwise = succ x
 
-  spred :: a -> a
-  spred x | x == minBound = maxBound
+        spred :: a -> a
+        spred x
+          | x == minBound = maxBound
           | otherwise = pred x
 
