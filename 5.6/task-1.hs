19ca19ff2a143e0fde21eb51b509f14282e11f6a
Nick Pavlov <n.pavlov@crpt.ru>
2019-10-31 20:21:05 +0300

    IO & Reader

---
+++ 5.6/task-1.hs
@@ -0,0 +1,18 @@
+data Reader r a = Reader { runReader :: (r -> a) }
+
+
+instance Functor (Reader r) where
+  fmap f a = undefined
+
+instance Applicative (Reader r) where
+  pure x = Reader $ \_ -> x
+
+instance Monad (Reader r) where
+  return x = Reader $ \_ -> x
+  m >>= k  = Reader $ \r -> runReader (k (runReader m r)) r
+
+local' :: (r -> r') -> Reader r' a -> Reader r a
+local' f m = undefined
+
+
+
